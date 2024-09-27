4d0f669b73cc99e7bbd5a0a0445f92fedd0e89e6
hgf700 <pawellapinski33@gmail.com>
2024-09-27 10:55:21 +0200

    scrapp with tables

--- scrapping.py
+++ scrapping.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from prettytable import PrettyTable
 
 # URL strony, którą chcesz scrapować
-url = 'https://www.scrapethissite.com/pages/simple/'
+url = 'https://www.scrapethissite.com/pages/forms/'
 
 # Wysyłanie żądania HTTP do serwera
 response = requests.get(url)
@@ -12,24 +13,30 @@ if response.status_code == 200:
     # Tworzenie obiektu BeautifulSoup do parsowania HTML
     soup = BeautifulSoup(response.content, 'lxml')
 
-    # Znajdowanie wszystkich elementów div z klasą country
-    countries = soup.find_all('div', class_='country')
-    #print(countries)
+    # Znajdowanie wszystkich elementów table z klasą table
+    teams = soup.find_all('tr', class_='team')
 
-for country in countries:
-    country_name = country.find('h3', class_='country-name').text.strip()
-    country_population = country.find('span', class_='country-population').text.strip()  
-    country_capitol = country.find('span', class_='country-capital').text.strip()  
-    country_area = country.find('span', class_='country-area').text.strip()  
+    # Tworzenie tabeli za pomocą PrettyTable
+    table = PrettyTable()
+    table.field_names = ["Team Name", "Year", "Wins", "Losses", "OT Losses", "Win %", "Goals For", "Goals Against", "+/-"]
 
-    print(f"Nazwa kraju: {country_name}")
-    print(f"Stolica: {country_capitol}")
-    print(f"Populacja: {country_population}")
-    print(f"Powierzchnia (km²): {country_area}")
-    print("-" * 40)  
+    # Iterowanie po drużynach
+    for team in teams:
+        team_name = team.find('td', class_='name').text.strip()
+        team_year = team.find('td', class_='year').text.strip()
+        team_wins = team.find('td', class_='wins').text.strip()
+        team_loses = team.find('td', class_='losses').text.strip()
+        team_otloses = team.find('td', class_='ot-losses').text.strip()
+        team_pct = team.find('td', class_='pct').text.strip()
+        team_gf = team.find('td', class_='gf').text.strip()
+        team_ga = team.find('td', class_='ga').text.strip()
+        team_diff = team.find('td', class_='diff').text.strip()
 
+        # Dodawanie wiersza do tabeli
+        table.add_row([team_name, team_year, team_wins, team_loses, team_otloses, team_pct, team_gf, team_ga, team_diff])
 
-    
+    # Wyświetlanie tabeli
+    print(table)
 
 else:
     print("Błąd: Nie udało się pobrać strony")
